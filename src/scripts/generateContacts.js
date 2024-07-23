a840937aa559946b9aa76411bb2dad3c24d2c88d
OleksandraTsy <aleksashka0581@gmail.com>
2024-07-23 16:24:14 +0200

    push hw-1

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,27 @@
+import * as fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    let contacts = JSON.parse(data);
+
+    for (let i = 0; i < number; i++) {
+      const newContact = createFakeContact();
+      contacts.push(newContact);
+    }
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(contacts, undefined, 2),
+      'utf-8',
+    );
+
+    console.log(`Successfully generated ${number} contacts.`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+generateContacts(5);
